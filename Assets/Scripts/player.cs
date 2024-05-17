d93e96571a002c67e9b4a579d7c5cb3e499721aa
Craig_HGP <craig@hiddengeniusproject.org>
2024-05-17 16:33:05 -0700

    more minor adjustments to the player script

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     public float speed = 10f; // Adjust the speed as needed
     public float rotationSpeed = 7f; // Adjust the rotation speed as needed
     public float pitchSpeed = 7f; // Adjust the pitch (up and down) speed as needed
-    public float pitchRange = 80f; // Adjust the maximum pitch angle (in degrees) as needed
+    public float pitchRange = 50f; // Adjust the maximum pitch angle (in degrees) as needed
     private Rigidbody rb;
     private float pitch = 0f;
     public float jumpForce = 100f; // Adjust the jump force as needed
